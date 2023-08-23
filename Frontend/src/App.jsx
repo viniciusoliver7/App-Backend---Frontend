3624b2ad7220c56d6907a5d5facb0a5f39b19d6a
LucasAvils <lalencar9@gmail.com>
2023-08-23 16:29:44 -0300

    Adição de uma tabela com mock data

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,22 @@
 import "./App.css";
-import "./Graphs/Pie"
+import "./Graphs/Pie";
 import Piechart from "./Graphs/Pie";
 import Barchart from "./Graphs/Bar";
+import Table from "./Graphs/Table/Table";
 
 function App() {
-
   return (
     <div className="box">
       <div className="Main">
         <h1>Projeto Dashboard</h1>
         <h1>Graphs</h1>
-          <div className="App">
-            <Piechart/>
-            <Barchart/>
-          </div>
+        <div className="App">
+          <Piechart />
+          <Barchart />
+        </div>
+        <div className="Table">
+          <Table />
+        </div>
       </div>
     </div>
   );
---
+++ Frontend/src/Graphs/Table/Table.jsx
@@ -0,0 +1,33 @@
+import MockData from "../../../MockData.json";
+import "./Table.css"
+
+export default function Table() {
+  const dados = MockData;
+
+  return (
+    <div className="tabela">
+    <table>
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Telefone</th>
+          <th>Email</th>
+          <th>Endereço</th>
+          <th>CEP</th>
+        </tr>
+      </thead>
+      <tbody>
+        {dados.map((item) => (
+          <tr key={item.name}>
+            <td>{item.name}</td>
+            <td>{item.phone}</td>
+            <td>{item.email}</td>
+            <td>{item.address}</td>
+            <td>{item.postalZip}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+    </div>
+  );
+}
